f493f3e32bbec2219a9890b78f3b84d9bc47d7b9
MisterC-Rogers <charles.rogers2024@gmail.com>
2020-01-18 18:12:04 -0600

    import sass

--- gatsby-bootcamp/src/Components/layout.js
+++ gatsby-bootcamp/src/Components/layout.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from '../Components/header'
 import Footer from '../Components/footer'
+import '../Styles/index.scss'
 
 export default function layout(props) {
     return (
